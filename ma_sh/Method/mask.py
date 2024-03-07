002c3958e1a598bdbc466ca41b95c48498f1b5dc
chLi <565353780@qq.com>
2024-03-07 18:00:25 +0800

    make py and c code format uniformly

--- ma_sh/Method/mask.py
+++ ma_sh/Method/mask.py
@@ -2,22 +2,26 @@ import torch
 
 
 def getMaskBaseValues(degree_max: int, phis: torch.Tensor) -> torch.Tensor:
-    base_values = [torch.ones_like(phis)]
+    base_values_list = [torch.ones_like(phis)]
 
     for degree in range(1, degree_max + 1):
         current_phis = degree * phis
-        base_values += [torch.cos(current_phis), torch.sin(current_phis)]
+        base_values_list.append(torch.cos(current_phis))
+        base_values_list.append(torch.sin(current_phis))
 
-    return torch.vstack(base_values)
+    base_values = torch.vstack(base_values_list)
+    return base_values
 
 
 def getMaskValues(
     phi_idxs: torch.Tensor, params: torch.Tensor, base_values: torch.Tensor
 ) -> torch.Tensor:
-    values = []
+    values_list = []
 
     for i in range(phi_idxs.shape[0] - 1):
         crop_base_values = base_values[:, phi_idxs[i]: phi_idxs[i + 1]]
-        values.append(torch.matmul(params[i], crop_base_values))
+        crop_values = torch.matmul(params[i], crop_base_values)
+        values_list.append(crop_values)
 
-    return torch.hstack(values)
+    values = torch.hstack(values_list)
+    return values
