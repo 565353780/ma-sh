b7fa572f185c77a94b199f530c444c46d3d951a2
chLi <565353780@qq.com>
2024-03-16 07:40:41 -0500

    add pointnet2 ops install

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,6 @@
 pip install torch torchvision torchaudio
 
-pip install .
+pip install ../ma-sh/ma_sh/Lib/pointnet2_ops_lib
+pip install ../ma-sh
 
 pip install tqdm open3d tensorboard matplotlib trimesh torchviz
