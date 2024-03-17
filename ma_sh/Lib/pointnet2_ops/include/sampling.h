a9f4271c78a9097d1b4b41243ff4c304beff5bc9
chLi <565353780@qq.com>
2024-03-17 06:26:38 -0500

    fix runtime import bug

--- ma_sh/Lib/pointnet2_ops/include/sampling.h
+++ ma_sh/Lib/pointnet2_ops/include/sampling.h
@@ -3,8 +3,9 @@
 #include <torch/extension.h>
 
 void furthest_point_sampling_kernel_wrapper(
-    int point_batch_num, int max_point_num, int max_sample_point_num,
-    const float *points, const int *point_counts, const int *point_start_idxs,
+    const int &point_batch_num, const int &max_point_num,
+    const int &max_sample_point_num, const float *points,
+    const int *point_counts, const int *point_start_idxs,
     const int *sample_point_start_idxs, float *tmp, int *idxs);
 
 const torch::Tensor
