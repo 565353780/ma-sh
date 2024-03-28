36c95108b0dc04a468b771d77995b14ae3aca19f
chLi <565353780@qq.com>
2024-03-28 22:27:46 +0800

    remove input data transform

--- ma_sh/Lib/ChamferDistance/chamfer_python.py
+++ ma_sh/Lib/ChamferDistance/chamfer_python.py
@@ -15,18 +15,19 @@ def NN_loss(x, y, dim=0):
     return values.mean()
 
 
-def batched_pairwise_dist(a, b):
-    x, y = a.double(), b.double()
+def batched_pairwise_dist(x, y):
     bs, num_points_x, points_dim = x.size()
     bs, num_points_y, points_dim = y.size()
 
     xx = torch.pow(x, 2).sum(2)
     yy = torch.pow(y, 2).sum(2)
     zz = torch.bmm(x, y.transpose(2, 1))
-    rx = xx.unsqueeze(1).expand(bs, num_points_y, num_points_x) # Diagonal elements xx
-    ry = yy.unsqueeze(1).expand(bs, num_points_x, num_points_y) # Diagonal elements yy
+    rx = xx.unsqueeze(1).expand(bs, num_points_y, num_points_x)  # Diagonal elements xx
+    ry = yy.unsqueeze(1).expand(bs, num_points_x, num_points_y)  # Diagonal elements yy
+
     P = rx.transpose(2, 1) + ry - 2 * zz
     return P
+
 
 def distChamfer(a, b):
     """
@@ -40,5 +41,9 @@ def distChamfer(a, b):
     Works for pointcloud of any dimension
     """
     P = batched_pairwise_dist(a, b)
-    return torch.min(P, 2)[0].float(), torch.min(P, 1)[0].float(), torch.min(P, 2)[1].int(), torch.min(P, 1)[1].int()
-
+    return (
+        torch.min(P, 2)[0].float(),
+        torch.min(P, 1)[0].float(),
+        torch.min(P, 2)[1].int(),
+        torch.min(P, 1)[1].int(),
+    )
