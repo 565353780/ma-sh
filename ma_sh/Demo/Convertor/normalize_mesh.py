efb4f54b53879b071491bf2629931a01080df528
chLi <565353780@qq.com>
2024-04-23 02:03:45 +0800

    set force start as default

--- ma_sh/Demo/Convertor/normalize_mesh.py
+++ ma_sh/Demo/Convertor/normalize_mesh.py
@@ -10,7 +10,7 @@ def demo():
     shape_root_folder_path = HOME + "/chLi/Dataset/SDF/ShapeNet/manifold/"
     save_root_folder_path = HOME + "/chLi/Dataset/Mash/ShapeNet/"
 
-    force_start = False
+    force_start = True
 
     convertor = Convertor(
         shape_root_folder_path,
