5a24cbe9716ac5ea60a0639434ee891105c8c443
chLi <565353780@qq.com>
2024-04-23 23:21:56 +0800

    add finish tag to save time

--- ma_sh/Test/data_check.py
+++ ma_sh/Test/data_check.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from ma_sh.Data.mesh import Mesh
 from ma_sh.Method.pcd import getPointCloud
+from ma_sh.Method.path import createFileFolder
 from ma_sh.Method.render import renderGeometries
 from ma_sh.Model.mash import Mash
 
@@ -15,6 +16,9 @@ def test():
     mesh_folder_path = dataset_folder_path + "Mash/ShapeNet/normalized_mesh/"
     mash_folder_path = dataset_folder_path + "Mash/ShapeNet/normalized_mash/"
     sdf_folder_path = dataset_folder_path + "SDF-Coarse/ShapeNet/sdf/"
+    tag_folder_path = dataset_folder_path + 'tag_data_check/'
+
+    os.makedirs(tag_folder_path, exist_ok=True)
 
     classname_list = os.listdir(mesh_folder_path)
 
@@ -45,57 +49,74 @@ def test():
                 + "/models/model_normalized_obj.npy"
             )
 
-            mesh = Mesh(mesh_file_path)
-            mash = Mash.fromParamsFile(mash_file_path, device="cpu")
-            sdf = np.load(sdf_file_path)
+            finish_tag_file_path = tag_folder_path + classname + '/' + modelid + '/finish.txt'
 
-            mash_points = mash.toSamplePoints().numpy()
-            sdf_points = sdf[sdf[:, 3] <= 0][:, :3]
+            if os.path.exists(finish_tag_file_path):
+                solved_shape_num += 1
+                print('solved_shape_num:', solved_shape_num)
+                continue
 
-            mesh_min_bound = np.min(mesh.vertices, axis=0)
-            mesh_max_bound = np.max(mesh.vertices, axis=0)
-            mesh_length = np.max(mesh_max_bound - mesh_min_bound)
-            mesh_center = (mesh_min_bound + mesh_max_bound) / 2.0
+            if os.path.exists(mesh_file_path):
+                mesh = Mesh(mesh_file_path)
 
-            mesh_center_error = np.linalg.norm(mesh_center)
-            mesh_length_error = np.abs(mesh_length - 0.9)
+                mesh_min_bound = np.min(mesh.vertices, axis=0)
+                mesh_max_bound = np.max(mesh.vertices, axis=0)
+                mesh_length = np.max(mesh_max_bound - mesh_min_bound)
+                mesh_center = (mesh_min_bound + mesh_max_bound) / 2.0
 
-            mash_min_bound = np.min(mash_points, axis=0)
-            mash_max_bound = np.max(mash_points, axis=0)
-            mash_length = np.max(mash_max_bound - mash_min_bound)
-            mash_center = (mash_min_bound + mash_max_bound) / 2.0
+                mesh_center_error = np.linalg.norm(mesh_center)
+                mesh_length_error = np.abs(mesh_length - 0.9)
 
-            mash_center_error = np.linalg.norm(mash_center)
-            mash_length_error = np.abs(mash_length - 0.9)
+                if mesh_center_error > 1e-6 or mesh_length_error > 1e-6:
+                    print("current: class:", classname, "model:", modelid)
+                    print("mesh error!")
+                    print("mesh:", mesh_center_error, mesh_length_error)
+                    error_info = 'class:' + classname + ';model:' + modelid + ';mesh\n'
 
-            sdf_min_bound = np.min(sdf_points, axis=0)
-            sdf_max_bound = np.max(sdf_points, axis=0)
-            sdf_length = np.max(sdf_max_bound - sdf_min_bound)
-            sdf_center = (sdf_min_bound + sdf_max_bound) / 2.0
+                    with open(error_file_path, 'a+') as f:
+                        f.write(error_info)
 
-            sdf_center_error = np.linalg.norm(sdf_center)
-            sdf_length_error = np.abs(sdf_length - 0.9)
+            if os.path.exists(mash_file_path):
+                mash = Mash.fromParamsFile(mash_file_path, device="cpu")
+                mash_points = mash.toSamplePoints().numpy()
 
-            error_info = ''
-            if mesh_center_error > 1e-6 or mesh_length_error > 1e-6:
-                print("current: class:", classname, "model:", modelid)
-                print("mesh error!")
-                print("mesh:", mesh_center_error, mesh_length_error)
-                error_info += 'class:' + classname + ';model:' + modelid + ';mesh\n'
-            if mash_center_error > 1e-2 or mash_length_error > 1e-2:
-                print("current: class:", classname, "model:", modelid)
-                print("mash error!")
-                print("mash:", mash_center_error, mash_length_error)
-                error_info += 'class:' + classname + ';model:' + modelid + ';mash\n'
-            if sdf_center_error > 1e-2 or sdf_length_error > 1e-2:
-                print("current: class:", classname, "model:", modelid)
-                print("sdf error!")
-                print("sdf:", sdf_center_error, sdf_length_error)
-                error_info += 'class:' + classname + ';model:' + modelid + ';sdf\n'
+                mash_min_bound = np.min(mash_points, axis=0)
+                mash_max_bound = np.max(mash_points, axis=0)
+                mash_length = np.max(mash_max_bound - mash_min_bound)
+                mash_center = (mash_min_bound + mash_max_bound) / 2.0
 
-            if error_info != '':
-                with open(error_file_path, 'w+') as f:
-                    f.write(error_info)
+                mash_center_error = np.linalg.norm(mash_center)
+                mash_length_error = np.abs(mash_length - 0.9)
+
+                if mash_center_error > 1e-2 or mash_length_error > 1e-2:
+                    print("current: class:", classname, "model:", modelid)
+                    print("mash error!")
+                    print("mash:", mash_center_error, mash_length_error)
+                    error_info = 'class:' + classname + ';model:' + modelid + ';mash\n'
+
+                    with open(error_file_path, 'a+') as f:
+                        f.write(error_info)
+
+            if os.path.exists(sdf_file_path):
+                sdf = np.load(sdf_file_path)
+                sdf_points = sdf[sdf[:, 3] <= 0][:, :3]
+
+                sdf_min_bound = np.min(sdf_points, axis=0)
+                sdf_max_bound = np.max(sdf_points, axis=0)
+                sdf_length = np.max(sdf_max_bound - sdf_min_bound)
+                sdf_center = (sdf_min_bound + sdf_max_bound) / 2.0
+
+                sdf_center_error = np.linalg.norm(sdf_center)
+                sdf_length_error = np.abs(sdf_length - 0.9)
+
+                if sdf_center_error > 1e-2 or sdf_length_error > 1e-2:
+                    print("current: class:", classname, "model:", modelid)
+                    print("sdf error!")
+                    print("sdf:", sdf_center_error, sdf_length_error)
+                    error_info = 'class:' + classname + ';model:' + modelid + ';sdf\n'
+
+                    with open(error_file_path, 'a+') as f:
+                        f.write(error_info)
 
             if False:
                 mash_pcd = getPointCloud(mash_points)
@@ -103,6 +124,9 @@ def test():
                 renderGeometries([mesh.toO3DMesh(), mash_pcd, sdf_pcd])
                 exit()
 
+            createFileFolder(finish_tag_file_path)
+            with open(finish_tag_file_path, 'w') as f:
+                f.write('\n')
             solved_shape_num += 1
             print('solved_shape_num:', solved_shape_num)
 
