312b6da4ec24a316ecc542ab2807bbd3c83e9efd
chLi <565353780@qq.com>
2024-04-24 21:30:20 +0800

    finish capture image convert

---
+++ ma_sh/Demo/Convertor/capture_image.py
@@ -0,0 +1,34 @@
+import os
+
+from ma_sh.Module.Convertor.capture_image import Convertor
+
+
+def demo():
+    HOME = os.environ["HOME"]
+
+    dataset_root_folder_path = HOME + "/chLi/Dataset/"
+    window_name = "Shape Image Sampler"
+    width = 224
+    height = 224
+    left = 10
+    top = 10
+    visible = False
+    y_rotate_num = 8
+    x_rotate_num = 5
+    force_start = False
+
+    convertor = Convertor(
+        dataset_root_folder_path,
+        window_name,
+        width,
+        height,
+        left,
+        top,
+        visible,
+        y_rotate_num,
+        x_rotate_num,
+        force_start,
+    )
+
+    convertor.convertAll()
+    return True
