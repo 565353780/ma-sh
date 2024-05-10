753b3e4e7132e8ff74030f6b34ce232fa03ef765
chLi <565353780@qq.com>
2024-05-10 22:36:31 +0800

    make convert mash always running to solve newly files

--- convert_mash.py
+++ convert_mash.py
@@ -1,4 +1,7 @@
+from time import sleep
 from ma_sh.Demo.Convertor.mash import demo as demo_convert_mash
 
 if __name__ == "__main__":
-    demo_convert_mash()
+    while True:
+        demo_convert_mash()
+        sleep(10)
