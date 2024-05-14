11506c0ad2177c2f4b3e128da1ace03883526ba2
chLi <565353780@qq.com>
2024-05-14 15:40:23 +0800

    use a relative small noise, since all objects are normalized

--- ma_sh/Demo/Convertor/sample_noise_pcd.py
+++ ma_sh/Demo/Convertor/sample_noise_pcd.py
@@ -3,12 +3,12 @@ import os
 from ma_sh.Module.Convertor.sample_noise_pcd import Convertor
 
 
-def demo():
+def demo(gauss_sigma: float = 0.01):
     HOME = os.environ["HOME"]
 
     dataset_root_folder_path = HOME + "/chLi/Dataset/"
     gt_points_num = 400000
-    gauss_sigma = 0.05
+    # gauss_sigma = 0.05
     force_start = False
 
     convertor = Convertor(
