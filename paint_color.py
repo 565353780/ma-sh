258e14cb2469e9a617875f0dce8aee4576676929
chLi <565353780@qq.com>
2024-05-18 13:22:32 +0800

    add save mesh call

--- paint_color.py
+++ paint_color.py
@@ -4,6 +4,7 @@ from ma_sh.Data.mesh import Mesh
 
 gt_mesh_file_path = './gt_mesh.ply'
 mash_pcd_file_path = './mash_pcd.ply'
+save_colored_gt_mesh_file_path = './colored_gt_mesh.ply'
 error_max_percent = 0.1
 accurate = True
 
@@ -13,3 +14,7 @@ mash_pcd = o3d.io.read_point_cloud(mash_pcd_file_path)
 mash_pts = np.asarray(mash_pcd.points)
 
 gt_mesh.paintJetColorsByPoints(mash_pts, error_max_percent, accurate)
+
+colored_gt_mesh = gt_mesh.toO3DMesh()
+
+o3d.io.write_triangle_mesh(save_colored_gt_mesh_file_path, colored_gt_mesh)
