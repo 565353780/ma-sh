2b684019f4ec508ae1e1bb189446a8452d4e270c
chLi <565353780@qq.com>
2024-05-28 15:42:31 +0800

    update fps metric calculate script

--- ma_sh/Test/metric.py
+++ ma_sh/Test/metric.py
@@ -1,25 +1,55 @@
 import os
 import torch
-import trimesh
+import numpy as np
+import open3d as o3d
 
 import mash_cpp
 
-mesh_folder_path = '/home/chli/chLi/Dataset/ManifoldMesh/ShapeNet/03001627/'
+from ma_sh.Config.shapenet import SHAPENET_NAME_DICT
 
 
-mesh_filename_list = os.listdir(mesh_folder_path)
-mesh_filename_list.sort()
+@torch.no_grad()
+def test():
+    HOME = os.environ["HOME"]
 
-for mesh_filename in mesh_filename_list:
-    mesh_file_path = mesh_folder_path + mesh_filename
+    category_id_list = list(SHAPENET_NAME_DICT.keys())
 
-    mesh = trimesh.load_mesh(mesh_file_path)
+    for category_id in category_id_list:
+        category_id = category_id_list[6]
+        mesh_folder_path = (
+            HOME + "/chLi/Dataset/ManifoldMesh/ShapeNet/" + category_id + "/"
+        )
 
-    pts1 = torch.from_numpy(mesh.sample(50000)).cuda()
-    pts2 = torch.from_numpy(mesh.sample(50000)).unsqueeze(0).cuda()
+        mesh_filename_list = os.listdir(mesh_folder_path)
+        mesh_filename_list.sort()
 
-    d1, d2 = mash_cpp.toChamferDistanceLoss(pts1, pts2)
-    cd = d1 + d2
+        cd_list = []
 
-    print(d1, d2, cd)
-    exit()
+        for mesh_filename in mesh_filename_list:
+            mesh_file_path = mesh_folder_path + mesh_filename
+
+            mesh = o3d.io.read_triangle_mesh(mesh_file_path)
+
+            pts1 = (
+                torch.from_numpy(
+                    np.asarray(mesh.sample_points_poisson_disk(50000).points)
+                )
+                .float()
+                .cuda()
+            )
+            pts2 = (
+                torch.from_numpy(
+                    np.asarray(mesh.sample_points_poisson_disk(50000).points)
+                )
+                .float()
+                .unsqueeze(0)
+                .cuda()
+            )
+
+            d1, d2 = mash_cpp.toChamferDistanceLoss(pts1, pts2)
+            cd = d1 + d2
+
+            cd_list.append(cd.item())
+
+            print(SHAPENET_NAME_DICT[category_id], np.mean(cd_list).item())
+    return True
