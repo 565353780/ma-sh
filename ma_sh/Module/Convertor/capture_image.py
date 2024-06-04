ba4865691a3e37b6cb1620f2aeb8803c6b040cd9
chLi <565353780@qq.com>
2024-06-04 15:33:09 +0800

    remove chair class constraint

--- ma_sh/Module/Convertor/capture_image.py
+++ ma_sh/Module/Convertor/capture_image.py
@@ -98,9 +98,6 @@ class Convertor(object):
         classname_list = os.listdir(dataset_folder_path)
         classname_list.sort()
         for classname in classname_list:
-            if classname != "03001627":
-                continue
-
             class_folder_path = dataset_folder_path + classname + "/"
 
             modelid_list = os.listdir(class_folder_path)
