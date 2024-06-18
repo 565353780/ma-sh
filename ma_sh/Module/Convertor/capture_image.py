a34adfff945b5778b4140dba0a10236f6413de7f
chLi <565353780@qq.com>
2024-06-18 22:44:10 +0800

    add zip file skip

--- ma_sh/Module/Convertor/capture_image.py
+++ ma_sh/Module/Convertor/capture_image.py
@@ -98,6 +98,9 @@ class Convertor(object):
         classname_list = os.listdir(dataset_folder_path)
         classname_list.sort()
         for classname in classname_list:
+            if '.zip' in classname:
+                continue
+            
             class_folder_path = dataset_folder_path + classname + "/"
 
             modelid_list = os.listdir(class_folder_path)
