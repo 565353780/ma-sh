c125d6617604c61981ee77643af4637bb3382750
chLi <565353780@qq.com>
2024-07-14 17:38:36 +0800

    remove \ on bat file

--- setup.bat
+++ setup.bat
@@ -5,7 +5,6 @@ pip install -U torch torchvision torchaudio
 cd ../ma-sh
 ./compile.sh
 
-pip install -U tqdm tensorboard matplotlib trimesh torchviz \
-	ftfy regex
+pip install -U tqdm tensorboard matplotlib trimesh torchviz ftfy regex
 
 pip install git+https://github.com/openai/CLIP.git
