833f768d31b42413ce37c47c92c75f3b935e3f2e
chLi <565353780@qq.com>
2024-10-21 21:42:55 +0800

    make sdf convert script auto convert all shapes

--- ma_sh/Demo/Convertor/sample_sdf.py
+++ ma_sh/Demo/Convertor/sample_sdf.py
@@ -6,13 +6,13 @@ import os
 from ma_sh.Module.Convertor.sample_sdf import Convertor
 
 
-def demo():
+def demo(gauss_noise: float = 0.0025):
     HOME = os.environ["HOME"]
 
     dataset_root_folder_path = HOME + "/chLi/Dataset/"
     sample_sdf_point_num = 250000
-    gauss_noise = 0.0025
-    force_start = False
+    # gauss_noise = 0.0025
+    force_start = True
 
     convertor = Convertor(
         dataset_root_folder_path,
