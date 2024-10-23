7514c9eb2cfee35dff9f5e100c4853a1c451248a
chLi <565353780@qq.com>
2024-10-23 15:21:06 +0800

    finish rotation and ortho6d test, need to use torch.float64 for accurate calculation!!!!

--- ma_sh/Method/rotate.py
+++ ma_sh/Method/rotate.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+import mash_cpp
+
 def toTriangleRotateMatrixs(points: torch.Tensor, triangles: np.ndarray) -> torch.Tensor:
     A = points[triangles[:, 0]]
     B = points[triangles[:, 1]]
@@ -21,3 +23,71 @@ def toTriangleRotateMatrixs(points: torch.Tensor, triangles: np.ndarray) -> torc
     triangle_rotate_matrixs = torch.stack([AB, regular_AC, N], dim=2)
 
     return triangle_rotate_matrixs
+
+
+# batch*n
+def normalize_vector(v):
+    batch = v.shape[0]
+    v_mag = torch.sqrt(v.pow(2).sum(1))  # batch
+    v_mag = torch.max(
+        v_mag, torch.autograd.Variable(torch.FloatTensor([1e-8]).cuda()))
+    v_mag = v_mag.view(batch, 1).expand(batch, v.shape[1])
+    v = v / v_mag
+    return v
+
+
+# u, v batch*n
+def cross_product(u, v):
+    batch = u.shape[0]
+    #print (u.shape)
+    #print (v.shape)
+    i = u[:, 1] * v[:, 2] - u[:, 2] * v[:, 1]
+    j = u[:, 2] * v[:, 0] - u[:, 0] * v[:, 2]
+    k = u[:, 0] * v[:, 1] - u[:, 1] * v[:, 0]
+
+    out = torch.cat((i.view(batch, 1), j.view(batch, 1), k.view(batch, 1)),
+                    1)  #batch*3
+
+    return out
+
+
+#poses batch*6
+#poses
+def compute_rotation_matrix_from_ortho6d(poses):
+    x_raw = poses[:, 0:3]  #batch*3
+    y_raw = poses[:, 3:6]  #batch*3
+
+    x = normalize_vector(x_raw)  #batch*3
+    z = cross_product(x, y_raw)  #batch*3
+    z = normalize_vector(z)  #batch*3
+    y = cross_product(z, x)  #batch*3
+
+    x = x.view(-1, 3, 1)
+    y = y.view(-1, 3, 1)
+    z = z.view(-1, 3, 1)
+    matrix = torch.cat((x, y, z), 2)  #batch*3*3
+    return matrix
+
+def toRegularRotateVectors(rotate_vectors: torch.Tensor) -> torch.Tensor:
+    rotate_matrixs = mash_cpp.toRotateMatrixs(rotate_vectors)
+
+    regular_rotate_vectors = mash_cpp.toRotateVectors(rotate_matrixs)
+
+    opposite_rotate_vector_idxs = torch.where(regular_rotate_vectors[:, 0] < 0)
+    regular_rotate_vectors[opposite_rotate_vector_idxs] = regular_rotate_vectors[opposite_rotate_vector_idxs] * -1.0
+    return regular_rotate_vectors
+
+def toOrthoPosesFromRotateVectors(rotate_vectors: torch.Tensor) -> torch.Tensor:
+    rotate_matrixs = mash_cpp.toRotateMatrixs(rotate_vectors)
+
+    ortho_poses = rotate_matrixs.permute(0, 2, 1).reshape(-1, 9)[:, :6]
+    return ortho_poses
+
+def toRotateVectorsFromOrthoPoses(ortho_poses: torch.Tensor) -> torch.Tensor:
+    rotate_matrixs = compute_rotation_matrix_from_ortho6d(ortho_poses)
+
+    rotate_vectors = mash_cpp.toRotateVectors(rotate_matrixs)
+
+    opposite_rotate_vector_idxs = torch.where(rotate_vectors[:, 0] < 0)
+    rotate_vectors[opposite_rotate_vector_idxs] = rotate_vectors[opposite_rotate_vector_idxs] * -1.0
+    return rotate_vectors
--- ma_sh/Test/rotate.py
+++ ma_sh/Test/rotate.py
@@ -1,8 +1,13 @@
 import torch
 import mash_cpp
 
+from ma_sh.Method.rotate import (
+    toRegularRotateVectors,
+    toOrthoPosesFromRotateVectors,
+    toRotateVectorsFromOrthoPoses
+)
 
-def test():
+def testFaceForwardRotation():
     a = torch.randn(1000, 3).type(torch.float64)
     a_norm = torch.norm(a, p=2, dim=1).reshape(-1, 1)
     a = a / a_norm
@@ -21,4 +26,58 @@ def test():
     max_error = torch.max(error)
 
     assert max_error < 1e-6
+    return True
+
+def testOrtho6D():
+    random_rotate_vectors = torch.randn((10000, 3), dtype=torch.float64, device='cuda')
+
+    regular_rotate_vectors = toRegularRotateVectors(random_rotate_vectors)
+
+    ortho_poses = toOrthoPosesFromRotateVectors(regular_rotate_vectors)
+
+    new_rotate_vectors = toRotateVectorsFromOrthoPoses(ortho_poses)
+
+    error = torch.norm(new_rotate_vectors - regular_rotate_vectors, dim=1)
+
+    error_idxs = torch.where(error > 1e-6)
+
+    if error_idxs[0].shape[0] > 0:
+        err = error[error_idxs]
+
+        error_rv0 = random_rotate_vectors[error_idxs]
+        error_rv1 = regular_rotate_vectors[error_idxs]
+        error_rv2 = new_rotate_vectors[error_idxs]
+
+        rot_mtx0 = mash_cpp.toRotateMatrixs(error_rv0)
+        rot_mtx1 = mash_cpp.toRotateMatrixs(error_rv1)
+        rot_mtx2 = mash_cpp.toRotateMatrixs(error_rv2)
+
+        print('error:')
+        print(err)
+
+        print('source_rotate_vectors:')
+        print(error_rv0)
+        print('regular_rotate_vectors:')
+        print(error_rv1)
+        print('new_rotate_vectors:')
+        print(error_rv2)
+
+        print('source_rotate_matrixs:')
+        print(rot_mtx0)
+        print('rotate_matrixs:')
+        print(rot_mtx1)
+        print('new_rotate_matrixs:')
+        print(rot_mtx2)
+        exit()
+
+    max_error = torch.max(error)
+
+    assert max_error < 1e-6
+    return True
+
+def test():
+    testFaceForwardRotation()
+
+    testOrtho6D()
+
     return True
