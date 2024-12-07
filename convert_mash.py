3ad4b024af20c7f2279a077e2abcae91493f8158
chLi <565353780@qq.com>
2024-12-07 12:59:24 +0000

    fix multi gpu running bug

--- convert_mash.py
+++ convert_mash.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from time import sleep
 from ma_sh.Demo.Convertor.mash import demo as demo_convert_mash
@@ -5,7 +6,7 @@ from ma_sh.Demo.Convertor.mash import demo as demo_convert_mash
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This is a sample program")
-    parser.add_argument('device', type=str, help="Input file")
+    parser.add_argument('device', default='cpu', type=str, help="Input file")
     args = parser.parse_args()
 
     while True:
---
+++ convert_mash.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=$1 python convert_mash.py cuda
