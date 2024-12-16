69c16d8b58dd4d2088ec09a723b7a30f471b9da0
chLi <565353780@qq.com>
2024-12-16 22:20:10 +0800

    upgrade sample pcd objaverse convert script

--- ma_sh/Demo/Convertor/sample_pcd_objaverse.py
+++ ma_sh/Demo/Convertor/sample_pcd_objaverse.py
@@ -4,20 +4,20 @@ from ma_sh.Module.Convertor.sample_pcd_objaverse import Convertor
 
 def demo():
     dataset_root_folder_path = toDatasetRootPath()
+    gt_points_num = 400000
+    source_data_type = '.obj'
+    target_data_type = '.npy'
+
     if dataset_root_folder_path is None:
         print('[ERROR][sample_pcd_objaverse::demo]')
         print('\t toDatasetRootPath failed!')
         return False
 
-    dataset_name = 'Objaverse_82K'
-    gt_points_num = 400000
-    worker_num = 1
-
     convertor = Convertor(
-        dataset_root_folder_path,
-        dataset_name,
+        dataset_root_folder_path + "/Objaverse_82K/manifold/",
+        dataset_root_folder_path + "/Objaverse_82K/manifold_pcd/",
         gt_points_num,
     )
 
-    convertor.convertAll(worker_num)
+    convertor.convertAll(source_data_type, target_data_type)
     return True
--- ma_sh/Module/Convertor/sample_pcd_objaverse.py
+++ ma_sh/Module/Convertor/sample_pcd_objaverse.py
@@ -1,114 +1,44 @@
-import os
 import numpy as np
-from tqdm import tqdm
-from multiprocessing import Pool
 
 from ma_sh.Data.mesh import Mesh
-from ma_sh.Method.path import createFileFolder, removeFile
+from ma_sh.Module.Convertor.base_convertor import BaseConvertor
 
 
-class Convertor(object):
+class Convertor(BaseConvertor):
     def __init__(
         self,
-        dataset_root_folder_path: str,
-        dataset_name: str,
+        source_root_folder_path: str,
+        target_root_folder_path: str,
         gt_points_num: int = 400000,
     ) -> None:
-        self.dataset_root_folder_path = dataset_root_folder_path
-        self.gt_points_num = gt_points_num
+        super().__init__(source_root_folder_path, target_root_folder_path)
 
-        self.manifold_mesh_folder_path = (
-            self.dataset_root_folder_path + dataset_name + "/manifold/"
-        )
-        self.sampled_pcd_folder_path = self.dataset_root_folder_path + dataset_name + "/manifold_pcd/"
+        self.gt_points_num = gt_points_num
         return
 
-    def convertOneShape(
-        self, model_id: str
-    ) -> bool:
-        rel_file_path = model_id
-
-        sampled_pcd_file_path = self.sampled_pcd_folder_path + rel_file_path + ".npy"
-
-        if os.path.exists(sampled_pcd_file_path):
-            return True
-
-        manifold_mesh_file_path = (
-            self.manifold_mesh_folder_path + rel_file_path + ".obj"
-        )
-
-        if not os.path.exists(manifold_mesh_file_path):
-            print("[ERROR][Convertor::convertOneShape]")
-            print("\t shape file not exist!")
-            print("\t manifold_mesh_file_path:", manifold_mesh_file_path)
-            return False
-
-        start_tag_file_path = self.sampled_pcd_folder_path + rel_file_path + "_start.txt"
-
-        if os.path.exists(start_tag_file_path):
-            return True
-
-        createFileFolder(start_tag_file_path)
-
-        with open(start_tag_file_path, "w") as f:
-            f.write("\n")
-
-        sampled_pcd_file_path = self.sampled_pcd_folder_path + rel_file_path + ".npy"
-
-        createFileFolder(sampled_pcd_file_path)
-
-        mesh = Mesh(manifold_mesh_file_path)
+    def convertData(self, source_path: str, target_path: str) -> bool:
+        mesh = Mesh(source_path)
 
         if not mesh.isValid():
-            print("[ERROR][Convertor::convertOneShape]")
+            print("[ERROR][Convertor::convertData]")
             print("\t mesh is not valid!")
-            print("\t manifold_mesh_file_path:", manifold_mesh_file_path)
+            print("\t source_path:", source_path)
             return False
 
         try:
             points = mesh.toSamplePoints(self.gt_points_num)
         except:
-            print("[ERROR][Convertor::convertOneShape]")
+            print("[ERROR][Convertor::convertData]")
             print("\t toSamplePoints failed!")
-            print("\t manifold_mesh_file_path:", manifold_mesh_file_path)
+            print("\t source_path:", source_path)
             return False
 
         if points is None:
-            print("[ERROR][Convertor::convertOneShape]")
+            print("[ERROR][Convertor::convertData]")
             print("\t toSamplePoints failed!")
-            print("\t manifold_mesh_file_path:", manifold_mesh_file_path)
+            print("\t source_path:", source_path)
             return False
 
-        np.save(sampled_pcd_file_path, points)
-
-        removeFile(start_tag_file_path)
-
-        return True
-
-    def convertAll(self, worker_num: int = 1) -> bool:
-        print("[INFO][Convertor::convertAll]")
-        print("\t start convert all shapes to mashes...")
-
-        dataset_folder_path = self.manifold_mesh_folder_path
-
-        classname_list = os.listdir(dataset_folder_path)
-        classname_list.sort()
-
-        for classname in classname_list:
-            class_folder_path = dataset_folder_path + classname + "/"
-
-            modelid_list = os.listdir(class_folder_path)
-            modelid_list.sort()
-
-            full_model_id_list = [classname + '/' + modelid[:-4] for modelid in modelid_list if not modelid.endswith('.txt')]
-
-            print("[INFO][Convertor::convertAll]")
-            print('\t start convert all objaverse meshes to sample pcd :', classname, '...')
-            with Pool(worker_num) as pool:
-                results = list(tqdm(
-                    pool.imap(self.convertOneShape, full_model_id_list),
-                    total=len(full_model_id_list),
-                    desc="Processing"
-                ))
+        np.save(target_path, points)
 
         return True
