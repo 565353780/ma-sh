aec992558682caa8ed283c7723613ba7c987bfe7
chLi <565353780@qq.com>
2024-12-16 22:44:54 +0800

    update normalize mesh script

--- ma_sh/Demo/Convertor/normalize_mesh.py
+++ ma_sh/Demo/Convertor/normalize_mesh.py
@@ -1,21 +1,21 @@
-import os
-
+from ma_sh.Config.custom_path import toDatasetRootPath
 from ma_sh.Module.Convertor.normalize_mesh import Convertor
 
 
 def demo():
-    HOME = os.environ["HOME"]
+    dataset_root_folder_path = toDatasetRootPath()
+    source_data_type = '.obj'
+    target_data_type = '.obj'
 
-    shape_root_folder_path = HOME + "/chLi/Dataset/ShapeNet/Core/ShapeNetCore.v2/"
-    dataset_root_folder_path = HOME + "/chLi/Dataset/"
-
-    force_start = False
+    if dataset_root_folder_path is None:
+        print('[ERROR][normalize_mesh::demo]')
+        print('\t toDatasetRootPath failed!')
+        return False
 
     convertor = Convertor(
-        shape_root_folder_path,
-        dataset_root_folder_path,
-        force_start,
+        dataset_root_folder_path + "Objaverse_82K/mesh/",
+        dataset_root_folder_path + "Objaverse_82K/normalized_mesh/",
     )
 
-    convertor.convertAll()
+    convertor.convertAll(source_data_type, target_data_type)
     return True
--- ma_sh/Module/Convertor/normalize_mesh.py
+++ ma_sh/Module/Convertor/normalize_mesh.py
@@ -1,74 +1,25 @@
-import os
 import numpy as np
 
 from ma_sh.Data.mesh import Mesh
-from ma_sh.Method.path import createFileFolder
+from ma_sh.Module.Convertor.base_convertor import BaseConvertor
 
 
-class Convertor(object):
+class Convertor(BaseConvertor):
     def __init__(
         self,
-        shape_root_folder_path: str,
-        dataset_root_folder_path: str,
-        force_start: bool = False,
+        source_root_folder_path: str,
+        target_root_folder_path: str,
     ) -> None:
-        self.shape_root_folder_path = shape_root_folder_path
-        self.dataset_root_folder_path = dataset_root_folder_path
-        self.force_start = force_start
-
-        self.normalized_mesh_folder_path = (
-            self.dataset_root_folder_path + "NormalizedMesh/"
-        )
-        self.tag_folder_path = self.dataset_root_folder_path + "Tag/NormalizedMesh/"
+        super().__init__(source_root_folder_path, target_root_folder_path)
         return
 
-    def convertOneShape(
-        self, dataset_name: str, class_name: str, model_id: str
-    ) -> bool:
-        rel_file_path = dataset_name + "/" + class_name + "/" + model_id
-
-        shape_file_path = (
-            self.shape_root_folder_path
-            + class_name
-            + "/"
-            + model_id
-            + "/models/model_normalized.obj"
-        )
-
-        if not os.path.exists(shape_file_path):
-            print("[ERROR][Convertor::convertOneShape]")
-            print("\t shape file not exist!")
-            print("\t shape_file_path:", shape_file_path)
-            return False
-
-        finish_tag_file_path = self.tag_folder_path + rel_file_path + "/finish.txt"
-
-        if os.path.exists(finish_tag_file_path):
-            return True
-
-        start_tag_file_path = self.tag_folder_path + rel_file_path + "/start.txt"
-
-        if os.path.exists(start_tag_file_path):
-            if not self.force_start:
-                return True
-
-        createFileFolder(start_tag_file_path)
-
-        with open(start_tag_file_path, "w") as f:
-            f.write("\n")
-
-        normalized_mesh_file_path = (
-            self.normalized_mesh_folder_path + rel_file_path + ".obj"
-        )
-
-        createFileFolder(normalized_mesh_file_path)
-
-        mesh = Mesh(shape_file_path)
+    def convertData(self, source_path: str, target_path: str) -> bool:
+        mesh = Mesh(source_path)
 
         if not mesh.isValid():
-            print("[ERROR][Convertor::convertOneShape]")
+            print("[ERROR][Convertor::convertData]")
             print("\t mesh is not valid!")
-            print("\t shape_file_path:", shape_file_path)
+            print("\t source_path:", source_path)
             return False
 
         min_bound = np.min(mesh.vertices, axis=0)
@@ -79,40 +30,6 @@ class Convertor(object):
 
         mesh.vertices = (mesh.vertices - center) * scale
 
-        mesh.save(normalized_mesh_file_path, True)
-
-        with open(finish_tag_file_path, "w") as f:
-            f.write("\n")
-
-        return True
-
-    def convertAll(self) -> bool:
-        print("[INFO][Convertor::convertAll]")
-        print("\t start convert all shapes to mashes...")
-        solved_shape_num = 0
-
-        classname_list = os.listdir(self.shape_root_folder_path)
-        classname_list.sort()
-        for classname in classname_list:
-            class_folder_path = self.shape_root_folder_path + classname + "/"
-
-            if not os.path.exists(class_folder_path):
-                continue
-
-            modelid_list = os.listdir(class_folder_path)
-            modelid_list.sort()
-
-            for modelid in modelid_list:
-                mesh_file_path = (
-                    class_folder_path + modelid + "/models/model_normalized.obj"
-                )
-
-                if not os.path.exists(mesh_file_path):
-                    continue
-
-                self.convertOneShape("ShapeNet", classname, modelid)
-
-                solved_shape_num += 1
-                print("solved shape num:", solved_shape_num)
+        mesh.save(target_path, True)
 
         return True
