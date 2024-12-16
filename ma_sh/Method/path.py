6eb1a082d26b5bf84894503d6e784872010bf313
chLi <565353780@qq.com>
2024-12-16 21:15:22 +0800

    make renameFile support overwrite control

--- ma_sh/Method/path.py
+++ ma_sh/Method/path.py
@@ -7,27 +7,24 @@ def createFileFolder(file_path):
     os.makedirs(file_folder_path, exist_ok=True)
     return True
 
-
-def renameFile(source_file_path, target_file_path):
-    if os.path.exists(target_file_path):
-        print('[ERROR][path::renameFile]')
-        print('\t target file already exist!')
-        print('\t source_file_path:', source_file_path)
-        print('\t target_file_path:', target_file_path)
-        return False
-
-    while os.path.exists(source_file_path):
-        try:
-            os.rename(source_file_path, target_file_path)
-        except:
-            pass
-    return True
-
-
 def removeFile(file_path):
     while os.path.exists(file_path):
         try:
             os.remove(file_path)
         except:
             continue
+    return True
+
+def renameFile(source_file_path, target_file_path, overwrite: bool = False):
+    if os.path.exists(target_file_path):
+        if not overwrite:
+            return True
+
+        removeFile(target_file_path)
+
+    while os.path.exists(source_file_path):
+        try:
+            os.rename(source_file_path, target_file_path)
+        except:
+            pass
     return True
