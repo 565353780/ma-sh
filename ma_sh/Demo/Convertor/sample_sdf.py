c1f799c7963f60bf07a50cd0735a0455bc8c1e24
chLi <565353780@qq.com>
2024-12-16 22:20:18 +0800

    simplify sample sdf script

--- ma_sh/Demo/Convertor/sample_sdf.py
+++ ma_sh/Demo/Convertor/sample_sdf.py
@@ -1,25 +1,30 @@
 import sys
-
 sys.path.append("../sdf-generate")
-import os
 
+from ma_sh.Config.custom_path import toDatasetRootPath
 from ma_sh.Module.Convertor.sample_sdf import Convertor
 
 
 def demo(gauss_noise: float = 0.0025):
-    HOME = os.environ["HOME"]
+    dataset_root_folder_path = toDatasetRootPath()
+    if dataset_root_folder_path is None:
+        print('[ERROR][sample_sdf::demo]')
+        print('\t toDatasetRootPath failed!')
+        return False
 
-    dataset_root_folder_path = HOME + "/chLi/Dataset/"
     sample_sdf_point_num = 250000
     # gauss_noise = 0.0025
-    force_start = False
+    source_data_type = '.obj'
+    target_data_type = '.npy'
+
+    noise_label = str(gauss_noise).replace('.', '_')
 
     convertor = Convertor(
-        dataset_root_folder_path,
+        dataset_root_folder_path + '/Objaverse_82K/manifold/',
+        dataset_root_folder_path + '/Objaverse_82K/sdf_' + noise_label + '/',
         sample_sdf_point_num,
         gauss_noise,
-        force_start,
     )
 
-    convertor.convertAll()
+    convertor.convertAll(source_data_type, target_data_type)
     return True
