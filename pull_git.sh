ab3f758cf0d537b552fcf2aa6ac06982ab8dc5f8
chLi <565353780@qq.com>
2024-12-17 16:50:34 +0800

    add pull git bash file

---
+++ pull_git.sh
@@ -0,0 +1,31 @@
+git pull
+
+if [ -d "../sdf-generate/" ]; then
+  cd ../sdf-generate/
+  git pull
+fi
+
+if [ -d "../open3d-manage/" ]; then
+  cd ../open3d-manage/
+  git pull
+fi
+
+if [ -d "../param-gauss-recon/" ]; then
+  cd ../param-gauss-recon/
+  git pull
+fi
+
+if [ -d "../open-clip-detect/" ]; then
+  cd ../open-clip-detect/
+  git pull
+fi
+
+if [ -d "../dino-v2-detect/" ]; then
+  cd ../dino-v2-detect/
+  git pull
+fi
+
+if [ -d "../ulip-manage/" ]; then
+  cd ../ulip-manage/
+  git pull
+fi
