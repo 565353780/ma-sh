5145edbfc66ebd1c269ae0aa804b16481ba2e62e
chLi <565353780@qq.com>
2024-12-17 17:05:12 +0800

    simplify clear tag script

--- clear_tag.py
+++ clear_tag.py
@@ -1,23 +1,25 @@
 import os
-from shutil import rmtree
+
+from ma_sh.Config.custom_path import toDatasetRootPath
+from ma_sh.Method.path import removeFile
 
 def clearTag(tag_folder_path: str) -> bool:
-    remove_folder_path_list = []
-
     for root, _, files in os.walk(tag_folder_path):
         for file in files:
-            if file != 'start.txt':
+            if not file.endswith('start.txt'):
                 continue
 
-            if not os.path.exists(root + '/finish.txt'):
-                remove_folder_path_list.append(root + '/')
-
-    for remove_folder_path in remove_folder_path_list:
-        rmtree(remove_folder_path)
+            removeFile(root + '/' + file)
 
     return True
 
 if __name__ == "__main__":
-    pcd_tag_folder_path = os.environ['HOME'] + '/chLi/Dataset/Tag/Objaverse_82K_pcd/'
+    dataset_root_folder_path = toDatasetRootPath()
+    if dataset_root_folder_path is None:
+        print('[ERROR][clear_tag::__main__]')
+        print('\t dataset not found!')
+        exit()
+
+    pcd_tag_folder_path = dataset_root_folder_path + 'Objaverse_82K/'
 
     clearTag(pcd_tag_folder_path)
