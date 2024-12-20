420f31820c676dac232a64414bc37fafeef6521b
chLi <565353780@qq.com>
2024-12-20 13:36:50 +0800

    upgrade the control pannel and button position

--- ma_sh/Module/aabb_selector.py
+++ ma_sh/Module/aabb_selector.py
@@ -11,12 +11,11 @@ class AABBSelector:
         self.pcd = mash.toSamplePcd()
         self.selected_points = None
 
-        # GUI Application setup
         gui.Application.instance.initialize()
 
         self.window = gui.Application.instance.create_window(
             "Mash AABB Selector", 1920, 1080)
-        
+
         em = self.window.theme.font_size
 
         self.scene = gui.SceneWidget()
@@ -28,7 +27,6 @@ class AABBSelector:
 
         self.scene.scene.add_geometry("MashPcd", self.pcd, self.material)
 
-        # Initial AABB
         points = np.asarray(self.pcd.points)
         aabb_min = np.min(points, axis=0)
         aabb_max = np.max(points, axis=0)
@@ -61,13 +59,8 @@ class AABBSelector:
         self.pannel.add_fixed(0.5*em)
         for i in range(3):
             self.pannel.add_child(self.sliders[i][0])
-            if i < 2:
-                self.pannel.add_fixed(0.25*em)
-
-        for i in range(3):
             self.pannel.add_child(self.sliders[i][1])
-            if i < 2:
-                self.pannel.add_fixed(0.25*em)
+            self.pannel.add_fixed(0.25*em)
 
         self.window.set_on_layout(self._on_layout)
         self.window.add_child(self.scene)
@@ -84,31 +77,39 @@ class AABBSelector:
             r.height, self.pannel.calc_preferred_size(
                 layout_context, gui.Widget.Constraints()).height
         )
-        self.pannel.frame = gui.Rect(r.get_right()-pannel_width,r.y,pannel_width,pannel_height)
+        self.pannel.frame = gui.Rect(
+            r.get_right()-pannel_width,
+            r.y,
+            pannel_width,
+            pannel_height)
 
         button_pref = self.select_button.calc_preferred_size(
             layout_context, gui.Widget.Constraints())
-        self.select_button.frame = gui.Rect(r.x,r.get_bottom()-button_pref.height, button_pref.width,button_pref.height)
+        self.select_button.frame = gui.Rect(
+            r.get_right()-pannel_width,
+            r.y+pannel_height,
+            pannel_width,
+            button_pref.height)
 
     def _create_aabb(self):
         min_bound = [self.sliders[i][0].double_value for i in range(3)]
         max_bound = [self.sliders[i][1].double_value for i in range(3)]
 
         corners = np.array([
-            [min_bound[0], min_bound[1], min_bound[2]],  # 0: min x, min y, min z
-            [max_bound[0], min_bound[1], min_bound[2]],  # 1: max x, min y, min z
-            [max_bound[0], max_bound[1], min_bound[2]],  # 2: max x, max y, min z
-            [min_bound[0], max_bound[1], min_bound[2]],  # 3: min x, max y, min z
-            [min_bound[0], min_bound[1], max_bound[2]],  # 4: min x, min y, max z
-            [max_bound[0], min_bound[1], max_bound[2]],  # 5: max x, min y, max z
-            [max_bound[0], max_bound[1], max_bound[2]],  # 6: max x, max y, max z
-            [min_bound[0], max_bound[1], max_bound[2]],  # 7: min x, max y, max z
+            [min_bound[0], min_bound[1], min_bound[2]],
+            [max_bound[0], min_bound[1], min_bound[2]],
+            [max_bound[0], max_bound[1], min_bound[2]],
+            [min_bound[0], max_bound[1], min_bound[2]],
+            [min_bound[0], min_bound[1], max_bound[2]],
+            [max_bound[0], min_bound[1], max_bound[2]],
+            [max_bound[0], max_bound[1], max_bound[2]],
+            [min_bound[0], max_bound[1], max_bound[2]],
         ])
 
         lines = np.array([
-            [0, 1], [1, 2], [2, 3], [3, 0],  # Bottom face
-            [4, 5], [5, 6], [6, 7], [7, 4],  # Top face
-            [0, 4], [1, 5], [2, 6], [3, 7]   # Vertical edges
+            [0, 1], [1, 2], [2, 3], [3, 0],
+            [4, 5], [5, 6], [6, 7], [7, 4],
+            [0, 4], [1, 5], [2, 6], [3, 7],
         ])
 
         line_set = o3d.geometry.LineSet()
@@ -148,7 +149,6 @@ class AABBSelector:
         self.selected_points = self.pcd.select_by_index(np.where(mask)[0])
         self.selected_points.paint_uniform_color([0, 1, 0])
 
-        # Add selected points to the scene
         self.scene.scene.remove_geometry("SelectedPoints")
         self.scene.scene.add_geometry("SelectedPoints", self.selected_points, self.material)
         self.scene.force_redraw()
