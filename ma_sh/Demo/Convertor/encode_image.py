4724230a08330c8f8f069e61694a922451a42e75
chLi <565353780@qq.com>
2024-12-26 23:28:46 +0800

    update model file paths

--- ma_sh/Demo/Convertor/encode_image.py
+++ ma_sh/Demo/Convertor/encode_image.py
@@ -3,28 +3,26 @@ sys.path.append('../open-clip-detect/')
 sys.path.append('../dino-v2-detect/')
 sys.path.append('../ulip-manage/')
 
-import os
-
 from ma_sh.Config.custom_path import toDatasetRootPath
 from ma_sh.Module.Convertor.encode_image import Convertor
 
 
 def demo():
     dataset_root_folder_path = toDatasetRootPath()
+    if dataset_root_folder_path is None:
+        print('[ERROR][encode_image::demo]')
+        print('\t toDatasetRootPath failed!')
+        return False
+
     model_file_path_dict = {
-        'clip': os.environ['HOME'] + '/chLi/Model/open_clip/DFN5B-CLIP-ViT-H-14-378.bin',
-        'dino': os.environ['HOME'] + '/chLi/Model/DINOv2/dinov2_vitl14_reg4_pretrain.pth',
-        'ulip': os.environ['HOME'] + '/chLi/Model/ULIP2/pretrained_models_ckpt_zero-sho_classification_pointbert_ULIP-2.pt',
+        'clip': dataset_root_folder_path + '../Model/open_clip/DFN5B-CLIP-ViT-H-14-378.bin',
+        'dino': dataset_root_folder_path + '../Model/DINOv2/dinov2_vitl14_reg4_pretrain.pth',
+        'ulip': dataset_root_folder_path + '../Model/ULIP2/pretrained_models_ckpt_zero-sho_classification_pointbert_ULIP-2.pt',
     }
     mode = 'dino'
     device = "cuda:0"
     source_data_type = '.png'
     target_data_type = '.npy'
-
-    if dataset_root_folder_path is None:
-        print('[ERROR][encode_image::demo]')
-        print('\t toDatasetRootPath failed!')
-        return False
 
     convertor = Convertor(
         dataset_root_folder_path + "Objaverse_82K/render/",
