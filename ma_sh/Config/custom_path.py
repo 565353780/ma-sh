94d88f342ccba8e4638dea5fcfce5a50663948c1
chLi <565353780@qq.com>
2024-12-29 10:09:26 +0000

    update dataset and model folder search order

--- ma_sh/Config/custom_path.py
+++ ma_sh/Config/custom_path.py
@@ -42,19 +42,15 @@ asdf_folder_path_dict = {
 }
 
 DATASET_ROOT_PATH_LIST = [
+    os.environ["HOME"] + "/chLi/Dataset/",
     os.environ["HOME"] + "/Dataset/",
     "/mnt/d/chLi/Dataset/",
-    os.environ["HOME"] + "/chLi/Dataset/",
-    # os.environ["HOME"] + "/Dataset/",
-    # "/mnt/data/jintian/chLi/Dataset/",
 ]
 
 MODEL_ROOT_PATH_LIST = [
+    os.environ["HOME"] + "/chLi/Model/",
     os.environ["HOME"] + "/Model/",
     "/mnt/d/chLi/Model/",
-    os.environ["HOME"] + "/chLi/Model/",
-    # os.environ["HOME"] + "/Dataset/",
-    # "/mnt/data/jintian/chLi/Dataset/",
 ]
 
 def toValidPath(path_list: list) -> Union[str, None]:
