33d9abebd87b69f4496fe1020607311b37a870f3
chLi <565353780@qq.com>
2025-01-02 20:43:56 +0800

    create sync code bash files for 8gpu and 4gpu servers

---
+++ sync_code_4gpu.sh
@@ -0,0 +1,20 @@
+sync_code() {
+  rsync -avP --exclude-from=$HOME/github/ASDF/$1/.gitignore --exclude='.git' --checksum --delete $HOME/github/ASDF/$1/ 4gpu:/home/jintian/github/$1/
+}
+
+rsync -avP --checksum $HOME/chLi/Dataset/Transformers 4gpu:/home/jintian/chLi/Dataset/
+
+sync_code ma-sh
+sync_code distribution-manage
+sync_code base-trainer
+sync_code mash-diffusion
+
+sync_code sdf-generate
+sync_code open3d-manage
+sync_code param-gauss-recon
+sync_code open-clip-detect
+sync_code dino-v2-detect
+sync_code ulip-manage
+
+sync_code CFM
+sync_code light-field-distance
