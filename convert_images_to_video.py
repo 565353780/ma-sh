938f0c05bb81fc006c6c250a27cd2883c1edd423
chLi <565353780@qq.com>
2025-01-11 22:40:22 +0800

    add keep_alive for service-mode running

--- convert_images_to_video.py
+++ convert_images_to_video.py
@@ -1,4 +1,12 @@
+from time import sleep
+
 from ma_sh.Demo.Convertor.images_to_video import demo as demo_convert_images_to_video
 
 if __name__ == '__main__':
-    demo_convert_images_to_video()
+    keep_alive = True
+
+    while True:
+        demo_convert_images_to_video()
+        if not keep_alive:
+            break
+        sleep(1)
