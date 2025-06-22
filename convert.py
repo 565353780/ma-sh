90b0180ba8ed9bd988f8e05efa6efcbb83714ee3
chLi <565353780@qq.com>
2025-06-22 18:34:21 +0800

    create the simple convert script

--- convert.py
+++ convert.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-from ma_sh.Method.path import removeFolder
-
 sys.path.append("../voxel-converter")
 sys.path.append("../chamfer-distance")
 sys.path.append("../data-convert")
@@ -23,10 +21,6 @@ if __name__ == "__main__":
     output_space = os.environ["HOME"] + "/chLi/Dataset/ShuMei/"
     rel_data_path = "0008dc75fb3648f2af4ca8c4d711e53e.npz"
     cuda_id = "0"
-
-    removeFolder(output_space + "mash/")
-    removeFolder("./logs/")
-    removeFolder("./output/")
 
     os.environ["CUDA_VISIBLE_DEVICES"] = cuda_id
     demo_convert_npz(data_space, output_space, rel_data_path)
