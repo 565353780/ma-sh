e06750fe76af1283f1502717fdf11ed98fd3297b
chLi <565353780@qq.com>
2025-07-14 19:02:41 +0800

    add to manifold script

---
+++ convert_manifold.py
@@ -0,0 +1,16 @@
+from ma_sh.Module.Convertor.to_manifold import Convertor as ToManifoldConvertor
+
+if __name__ == "__main__":
+    mesh_folder_path = "/home/chli/chLi/Dataset/ShapeNet/mesh/"
+    manifold_folder_path = "/home/chli/chLi/Dataset/ShapeNet/manifold/"
+    source_data_type = ".obj"
+    target_data_type = ".ply"
+    output_freq = 1.0
+
+    to_manifold_convertor = ToManifoldConvertor(
+        mesh_folder_path,
+        manifold_folder_path,
+        depth=8,
+    )
+
+    to_manifold_convertor.convertAll()
